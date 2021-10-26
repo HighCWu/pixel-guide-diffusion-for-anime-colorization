adc62b7af0d1882767d34dfa8996ca0440c91ca9
Hecong Wu <HighCWu@163.com>
2021-10-26 14:49:22 +0000

    update

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="pixel-guide-diffusion",
     py_modules=["pixel_guide_diffusion"],
-    # install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
